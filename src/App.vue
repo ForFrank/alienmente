cb8208c2277ef2829ce81e0d52b31d9d33ae5f7f
梁振奇 <liangzhenqi@hotmail.com>
2021-12-28 23:39:13 +0800

    fix: 样式问题

--- src/App.vue
+++ src/App.vue
@@ -29,4 +29,35 @@ body {
   width: 1000px;
   margin: 10px auto;
 }
+.el-header,
+.el-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
+.el-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+.el-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  line-height: 160px;
+}
+body > .el-container {
+  margin-bottom: 40px;
+}
+.el-container:nth-child(5) .el-aside,
+.el-container:nth-child(6) .el-aside {
+  line-height: 260px;
+}
+.el-container:nth-child(7) .el-aside {
+  line-height: 320px;
+}
 </style>
